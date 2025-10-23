173482ef0cbd8efd0638fb6e87a6ba78be93bea4
Katsiaryna Davydzenka <katsiarynadavydzenka@cli-10-109-0-121.WIFIeduroamEXT.units.it>
2025-10-23 11:01:35 +0200

    minor updates

--- muscle_case_study/utils/utils.R
+++ muscle_case_study/utils/utils.R
@@ -191,10 +191,10 @@ perform_analysis_rna <- function(input_data,
 
     metadata$patient <- as.numeric(as.factor(metadata$sample))
     sf <- devil:::calculate_sf(counts)
-    data_g = group_cell(count = counts, id = metadata$patient, pred = design_matrix, offset = sf)
-    print(str(data_g))
-    fit <- nebula::nebula(data_g$count, id = data_g$id, pred = data_g$pred, offset = data_g$offset)
-    #fit <- nebula::nebula(counts, id = metadata$patient, pred = design_matrix, offset = sf)
+    #data_g = group_cell(count = counts, id = metadata$patient, pred = design_matrix, offset = sf)
+    #print(str(data_g))
+    #fit <- nebula::nebula(data_g$count, id = data_g$id, pred = data_g$pred, offset = data_g$offset)
+    fit <- nebula::nebula(counts, id = metadata$patient, pred = design_matrix, offset = sf)
     res <- fit$summary
     #res <- dplyr::tibble(
       #name = fit$summary$gene,
