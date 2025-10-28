80a4ebcbcfb1167052d7dd2f8c1cce1f9a9c9234
Katsiaryna Davydzenka <katiasen89@gmail.com>
2025-10-28 18:44:34 +0100

    mc

--- muscle_case_study/01_deTest.R
+++ muscle_case_study/01_deTest.R
@@ -21,7 +21,7 @@ input_data <- read_data(dataset_name, data_path)
 input_data <- prepare_rna_input(input_data)
 
 # Define parameters
-methods <- c("glmGamPoi")
+methods <- c("nebula")
 design_tests <- c("age_type1", "age_type2", "interaction")
 path_fit_res <- "/orfeo/cephfs/scratch/cdslab/kdavydzenka/sc_devil/results/muscle/new/"
 
--- muscle_case_study/run_de_test.sh
+++ muscle_case_study/run_de_test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --partition=THIN
-#SBATCH --mem=200GB
+#SBATCH --mem=50GB
 #SBATCH --time=8:00:00
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --output=out.log
-#SBATCH --job-name=de_analysis
+#SBATCH --job-name=de_test
 
 module load R/4.4.1
 
