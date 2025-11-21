9a6df8ad1fbf5835657f76e65f193e876774c0eb
Katsiaryna Davydzenka <katiasen89@gmail.com>
2025-11-21 16:55:53 +0100

    update results subsampled

--- muscle_case_study/utils/utils.R
+++ muscle_case_study/utils/utils.R
@@ -129,7 +129,7 @@ fit_de <- function(input_data,
       parallel.cores = 1,
       verbose = TRUE, 
       init_overdispersion = NULL, 
-      max_iter = 600, 
+      max_iter = 800, 
       tolerance = 1e-3, 
       batch_size = 1
     )
