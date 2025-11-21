11dca0355f7577187b462e7bb05e5b0f5a2338bf
Katsiaryna Davydzenka <katiasen89@gmail.com>
2025-11-21 14:14:53 +0100

    update results fit

--- muscle_case_study/01_fitData.R
+++ muscle_case_study/01_fitData.R
@@ -20,7 +20,7 @@ input_data <- read_data(dataset_name, data_path)
 input_data <- prepare_rna_input(input_data)
 
 # Define parameters
-methods <- c("nebula")
+methods <- c("devil")
 design_tests <- c("age_type1", "age_type2", "interaction")
 
 
